55c3fce0da256d8c14c41a8034447f871ba6b647
KiwiUG <kiwiug69@gmail.com>
2025-08-28 22:09:58 +0530

    Initial Commit

---
+++ ClipNotch/ClipNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipNotchApp.swift
+//  ClipNotch
+//
+//  Created by Utsav Gautam on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipNotchTests/ClipNotchTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipNotchTests.swift
+//  ClipNotchTests
+//
+//  Created by Utsav Gautam on 28/08/25.
+//
+
+import Testing
+@testable import ClipNotch
+
+struct ClipNotchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
